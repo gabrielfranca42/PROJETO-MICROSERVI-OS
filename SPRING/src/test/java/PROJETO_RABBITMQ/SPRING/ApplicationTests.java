c65a2b82a6730d9083012fb33b6d6783d55931cf
gabrielfranca42 <gabrielfranca172@gmail.com>
2025-09-26 10:49:20 -0300

    xc

---
+++ SPRING/src/test/java/PROJETO_RABBITMQ/SPRING/ApplicationTests.java
@@ -0,0 +1,13 @@
+package PROJETO_RABBITMQ.SPRING;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
